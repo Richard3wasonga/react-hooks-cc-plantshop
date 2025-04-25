ac2637a3b03610ab183edfd1d55c93073c586a80
richard <richard.wasonga@student.moringaschool.com>
2025-04-25 15:08:24 +0300

    complete deliverables

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React,{useState} from "react";
 
 function NewPlantForm() {
+  const [name, setname] = useState('')
+  const [image, setimage] = useState('')
+  const [price, setprice] = useState('')
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+
+    const newPlant = {
+      name: name,
+      image: image,
+      price: price,
+    }
+
+    
+      fetch('http://localhost:6001/plants',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPlant)
+  
+      })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error))
+  
+    
+
+    setname('')
+    setimage('')
+    setprice('')
+  }
+
+  
+  
+
+  const handleName = (e) => {
+    setname(e.target.value)
+  }
+  const handleImage = (e) => {
+    setimage(e.target.value)
+  }
+  const handlePrice = (e) => {
+    setprice(e.target.value)
+  }
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" value={name} onChange={handleName}/>
+        <input type="text" name="image" placeholder="Image URL" value={image} onChange={handleImage}/>
+        <input type="number" name="price" step="0.01" placeholder="Price" value={price} onChange={handlePrice}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 function PlantCard() {
+  const [plantInfo, setplantInfo] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(res => res.json())
+    .then(data => setplantInfo(data))
+    .catch(error => console.error(error))
+  }, [])
+  
   return (
-    <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+    <ul>
+    {plantInfo.map((plant) => (
+      <li className="card" data-testid="plant-item" key={plant.id}>
+      <img src={plant.image || "https://via.placeholder.com/400"} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
     </li>
+
+    ))}
+    </ul>
+    
   );
 }
 
