d118761726fe97358c9e7f3c4c30091a154e7ddf
richard <richard.wasonga@student.moringaschool.com>
2025-04-25 15:29:39 +0300

    added delete functionality

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -8,6 +8,16 @@ function PlantCard() {
     .then(data => setplantInfo(data))
     .catch(error => console.error(error))
   }, [])
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:6001/plants/${id}`,{
+      method: "DELETE",
+    })
+    .then(res => res.json())
+    .then(data => console.LOG(data))
+    .catch(error => console.error(error))
+
+  }
   
   return (
     <ul>
@@ -16,6 +26,7 @@ function PlantCard() {
       <img src={plant.image || "https://via.placeholder.com/400"} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
+      <button className="deletebtn" onClick={() => handleDelete(plant.id)}>Delete</button>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
